6ca8b044e374157cbaec1c2de15fdff97aeb97b3
turukmankto <vlad_denisenko13@mail.ru>
2023-10-18 13:27:32 +0300

    improved code

--- date_stuff.py
+++ date_stuff.py
@@ -11,6 +11,10 @@ def get_date(days_delta=0, edit_format=True, day_start_from=date.today()):
 
 
 def month_name(month=1):
+    if not isinstance(month, int):
+        return 'err'
+    if month not in months:
+        return 'err'
     months = {1: "Январь", 2: "Февраль", 3: "Март", 4: "Апрель", 5: "Май", 6: "Июнь", 7: "Июль", 8: "Август", 9: "Сентябрь", 10: "Октябрь", 11: "Ноябрь", 12: "Декабрь"}
     return months[month]
 
@@ -18,6 +22,10 @@ def month_name(month=1):
 # TDD
 def month_short_name(month=1):
     months = {1: "янв", 2: "фев", 3: "мар", 4: "апр", 5: "май", 6: "июн", 7: "июл", 8: "авг", 9: "сен", 10: "окт", 11: "ноя", 12: "дек"}
+    if not isinstance(month, int):
+        return 'err'
+    if month not in months:
+        return 'err'
     return months[month]
 
 
